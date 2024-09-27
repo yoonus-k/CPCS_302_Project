5a710a9812f3ebca58cd8d0c27131fa98a6f55da
YOONUS KIZHAKKETHIL <131618962+yoonus-k@users.noreply.github.com>
2024-09-27 19:09:17 +0300

    added java tokenizer folder and files

---
+++ Code/Java/src/Tokenizer.java
@@ -0,0 +1,346 @@
+import java.io.*;
+
+public class Tokenizer {
+
+    static char lookahead; // To store the current character being read
+
+    public static void main(String[] args) {
+        tokenizer();
+        System.out.println("Tokens have been identified successfully....!!!");
+    }
+
+    public static void tokenizer() {
+        // Reserved words of C/Java into an array
+        String[] reserveWords = {
+                "int", "float", "if", "else", "while", "for", "abstract", "continue", "new",
+                "switch", "assert", "default", "package", "synchronized", "boolean", "do",
+                "private", "this", "break", "double", "protected", "implements", "throw",
+                "byte", "return", "import", "public", "throws", "case", "enum",
+                "instanceof", "transient", "catch", "extends", "short", "try", "char",
+                "final", "interface", "static", "void", "class", "finally", "long",
+                "strictfp", "volatile", "const", "native", "super", "while"
+        };
+
+        // Array to store lexemes
+        char[] lexeme = new char[30];
+        int state = 0;
+        int i = 0;
+
+        // Use PushbackReader to allow unreading characters
+        try (PushbackReader inputFile = new PushbackReader(new FileReader("input.txt"), 1);
+             PrintWriter outputFile = new PrintWriter(new FileWriter("output.txt"))) {
+
+            // Read one character at a time from the input file
+            while (inputFile.ready()) {
+
+                switch (state) {
+                    case 0:
+                        lookahead = (char) inputFile.read();
+                        if (Character.isWhitespace(lookahead)) {
+                            // Ignore whitespace
+                        } else if (lookahead == '>') {
+                            state = 1;
+                            lexeme[i++] = lookahead;
+                        } else if (lookahead == '<') {
+                            state = 4;
+                            lexeme[i++] = lookahead;
+                        } else if (lookahead == '=') {
+                            state = 7;
+                            lexeme[i++] = lookahead;
+                        } else if (lookahead == '!') {
+                            state = 10;
+                            lexeme[i++] = lookahead;
+                        } else if (Character.isLetter(lookahead) | lookahead == '$' | lookahead == '_') {
+                            state = 13;
+                            lexeme[i++] = lookahead;
+                        } else if (lookahead == '+') {
+                            state = 15;
+                            lexeme[i++] = lookahead;
+                        } else if (lookahead == '-') {
+                            state = 19;
+                            lexeme[i++] = lookahead;
+                        } else if (lookahead == '*') {
+                            state = 23;
+                            lexeme[i++] = lookahead;
+                        } else if (lookahead == '%') {
+                            state = 32;
+                            lexeme[i++] = lookahead;
+                        } else if (lookahead == '|') {
+                            state = 35;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == '&') {
+                            state = 38;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == '"') {
+                            state = 41;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == ';') {
+                            state = 47;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == ':') {
+                            state = 48;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == ',') {
+                            state = 49;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == '{') {
+                            state = 52;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == '}') {
+                            state = 53;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == '[') {
+                            state = 54;
+                            lexeme[i++] = lookahead;
+                        }else if (lookahead == ']') {
+                            state = 55;
+                            lexeme[i++] = lookahead;
+                        }else if (Character.isDigit(lookahead)) {
+                            state = 63;
+                            lexeme[i++] = lookahead;
+                        }else {
+                            error();
+                        }
+                        break;
+
+                    case 1: // Handles '>'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '=') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: greater_eq_op");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: greater_op");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 4: // Handles '<'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '=') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: less_eq_op");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: less_op");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 7: // Handles '='
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '=') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: relational_eq");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: assign_op");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 10: // Handles '!'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '=') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: relational_not_eq");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: logical_NOT");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 13: // Handles '&'
+                        lookahead = (char) inputFile.read();
+                        if (Character.isLetter(lookahead) | lookahead == '$' | lookahead == '_' | Character.isDigit(lookahead)) {
+                            lexeme[i++] = lookahead;
+                            state = 13;
+                            break;
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: id");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 15: // Handles '+'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '=') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: add_assign");
+                        } else if (lookahead == '+') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: inc");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: arithmetic_add");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 19: // Handles '-'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '=') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: subtract_assign");
+                        } else if (lookahead == '-') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: Dec");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: arithmetic_subtraction");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 23: // Handles '*'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '=') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: multiply_assign");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: arithmetic_multiplication_op");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 32: // Handles '%'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '=') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: mod_assign");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: arithmetic_mod_op");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 35: // Handles '|'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '|') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: logical_OR");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: vertical_bar");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    case 38: // Handles '&'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '&') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: logical_AND");
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: ampersand");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 41: // Handles '"'
+                        lookahead = (char) inputFile.read();
+                        if (lookahead == '"') {
+                            lexeme[i++] = lookahead;
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: String");
+                        } else {
+                            lexeme[i++] = lookahead;
+                            state = 41;
+                            break;
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 47: // Handles ';'
+                        outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: semi_colon");
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 48: // Handles ';'
+                        outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: colon");
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 49: // Handles ';'
+                        outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: comma");
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 52: // Handles ';'
+                        outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: Open_curly");
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 53: // Handles ';'
+                        outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: close_curly");
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 54: // Handles ';'
+                        outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: open_bracket");
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 55: // Handles ';'
+                        outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: close_bracket");
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 63: // Handles 'Digits'
+                        lookahead = (char) inputFile.read();
+                        if (Character.isDigit(lookahead)) {
+                            lexeme[i++] = lookahead;
+                            state = 63;
+                            break;
+                        } else if (lookahead == '.') {
+                            lexeme[i++] = lookahead;
+                            state = 65;
+                            break;
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: Int");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+                    case 65: // Handles '&'
+                        lookahead = (char) inputFile.read();
+                        if (Character.isDigit(lookahead)) {
+                            lexeme[i++] = lookahead;
+                            state = 65;
+                            break;
+                        } else {
+                            inputFile.unread(lookahead); // Unread the character
+                            outputFile.println("Lexeme: " + String.valueOf(lexeme, 0, i) + ", Token: float_point");
+                        }
+                        i = 0; // Reset lexeme
+                        state = 0;
+                        break;
+
+                    // Add more cases as necessary for other operators
+                }
+            }
+            outputFile.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void error() {
+        System.out.println("Error: UNRECOGNIZED_TOKEN");
+    }
+}
